a8892822ddb642bf80a2b7c273cca7cb3bc17698
Bryan Parthum <parthum.brayn@epa.gov>
2020-11-05 16:26:45 -0500

    Update to include DICE2016

--- mimi_analyze.jl
+++ mimi_analyze.jl
@@ -3,12 +3,12 @@
 ######################################
 
 # # Add Mimi
-# import Pkg;
-# Pkg.add("Mimi")
+import Pkg;
+Pkg.add("Mimi")
 
 # Update mimi
-# Pkg.update();
-# Pkg.update("Mimi")
+Pkg.update();
+Pkg.update("Mimi")
 
 # Use Mimi to load dependancies etc.
 using Mimi
@@ -17,25 +17,48 @@ using Mimi
 # registry add https://github.com/mimiframework/MimiRegistry.git
 
 ######################################
-################################  DICE
+###########################  DICE 2010
 ######################################
 
 # Add DICE
-# Pkg.add("MimiDICE2010")
-# Pkg.add("MimiDICE2016")
+Pkg.add("MimiDICE2010")
 
+# Use
 using MimiDICE2010
+
+# Definitions
 dice10 = MimiDICE2010.get_model()
-scc = MimiDICE2010.compute_scc(year = 2025)
 
-# using MimiDICE2016
-# dice16 = MimiDICE2016.get_model()
-
-# RUN
+# Run
 run(dice10)
 
 # EXPLORE
-explore(dice10, title = "My Window")
+explore(dice10, title = "Dice 2010")
+
+# Social Cost of Co2
+dice10_scc2015 = MimiDICE2010.compute_scc(year = 2015)
+
+######################################
+###########################  DICE 2016
+######################################
+
+# Add DICE (not yet in MimiRegistry)
+Pkg.add(url="https://github.com/AlexandrePavlov/MimiDICE2016.jl")
+
+# Use
+using MimiDICE2016
+
+# Definitions
+dice16 = MimiDICE2016.get_model()
+
+# Run
+run(dice16)
+
+# EXPLORE
+explore(dice16, title = "Dice 2016")
+
+# Social Cost of Co2
+dice16_scc2015 = MimiDICE2016.compute_scc(year = 2015)
 
 ######################################
 ################################  FUND
@@ -54,5 +77,8 @@ run(fund)
 # EXPLORE
 fun = fund[:socioeconomic, :income]
 explore(fund, title = "My Window")
+
+
+
 
 ## END OF SCRIPT. Have a great day!
